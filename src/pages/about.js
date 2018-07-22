03511d51bd789b072645002e8005d5e0fdcdcf00
Jeffrey Kwan <jeffrey.kwan@zenpayroll.com>
2018-07-22 13:14:36 -0700

    about

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/layout';
+
+const AboutPage = () => (
+  <Layout>
+    <p>
+      Hi! Thanks for checking out my work. Photography is a new hobby for me.
+    </p>
+  </Layout>
+);
+
+export default AboutPage;
